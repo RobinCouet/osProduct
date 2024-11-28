281a60f3fdbde5098da44f075ddf6f3e8cc985f6
Robin Couet <robin@start-win.fr>
2024-11-28 14:41:16 +0100

    add front url env

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://localhost:27017/prepavenir")
     })
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: process.env.FRONT_URL
 }));
 
 app.use(express.json());
